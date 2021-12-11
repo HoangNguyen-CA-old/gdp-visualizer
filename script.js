0c2503d78495ab939d23f10296833a1d6380aa1e
Hoang <hoang.viet.nguyen@outlook.com>
2021-12-11 13:58:42 -0500

    added d3 chart using 4 different scales, gets data from API and displays it in bar chart

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-const data = [1, 2, 3, 4, 5];
-d3.select('body')
-  .selectAll('p')
-  .data(data)
-  .enter()
-  .append('p')
-  .text((d) => d);
+const w = 1500;
+const h = 1000;
+const svg = d3.select('svg').attr('width', w).attr('height', h);
+const padding = 50;
 
-console.log(data);
+window.addEventListener('DOMContentLoaded', () => {
+  fetch(
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const values = data.data;
+
+      const xScale = d3
+        .scaleLinear()
+        .domain([0, values.length - 1])
+        .range([padding, w - padding]);
+
+      const heightScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(values, (d) => d[1])])
+        .range([0, h - 2 * padding]);
+
+      const yAxisScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(values, (d) => d[1])])
+        .range([h - padding, padding]);
+
+      const xAxisScale = d3
+        .scaleTime()
+        .domain([
+          new Date(values[0][0]),
+          new Date(values[values.length - 1][0]),
+        ])
+        .range([padding, w - padding]);
+
+      const xAxis = d3.axisBottom(xAxisScale);
+      const yAxis = d3.axisLeft(yAxisScale);
+
+      svg
+        .selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => xScale(i))
+        .attr('y', (d) => h - padding - heightScale(d[1]))
+        .attr('width', (w - 4 * padding) / values.length)
+        .attr('height', (d) => heightScale(d[1]));
+
+      svg
+        .append('g')
+        .attr('transform', `translate(0, ${h - padding})`)
+        .call(xAxis);
+      svg.append('g').attr('transform', `translate(${padding}, 0)`).call(yAxis);
+    });
+});
